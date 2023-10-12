be78a74117e6b4383633a697fd0b8e03663891ae
pablodKampmann <pablo7kamp@gmail.com>
2023-10-12 17:02:34 -0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -95,8 +95,12 @@ export default function Page() {
     }, 5000);
   }
 
+  function handleSetAppoint() {
+    console.log("hola");
+  }
+
   return (
-    <div className="ml-72 p-4 mt-20 mr-10 relative">
+    <div className="ml-64 p-4 mt-20 relative">
       <div>
         {openModalCreatePatient && (
           <div className="fixed inset-0 backdrop-blur-sm ml-64 z-10">
@@ -109,7 +113,7 @@ export default function Page() {
           <h1 className='font-bold text-lg text-teal-900'>Jueves 10 de Noviembre (2023)</h1>
         </div>
         <div>
-          eoiefjnoq
+          puto el que lee
         </div>
         <div>
           <button onClick={() => { setShowForm(!showForm); setPatient(null); setSearchContent(''); setDate(null) }} className="shadow-xl h-10 bg-teal-500 hover:bg-teal-900 hover:border-teal-600 text-white text-xl font-semibold py-2  px-12 border-b-4 border-teal-700 rounded-lg flex items-center transition duration-200">
@@ -126,42 +130,37 @@ export default function Page() {
           </button>
         </div>
       </div>
-      <div className='flex justify-between'>
-        <div className='border-4 border-teal-500 rounded-2xl mr-2 shadow-xl flex-1 h-screen overflow-auto'>
-          <div className='h-3/6'>
+      <div className='flex justify-between '>
+        <div className='border-4 border-teal-500 rounded-2xl shadow-xl flex-1 max-h-[33rem] w-8/12	overflow-y-auto'>
+          <div className=''>
             <table className='w-full'>
               <thead>
                 <tr className='bg-gray-500 text-center cursor-default	'>
-                  <th className='border-2 border-teal-500 p-3 rounded-tl-xl'>Tiempo</th>
-                  <th className='border-2 border-teal-500 p-3 '>Lunes</th>
-                  <th className='border-2 border-teal-500 p-3 '>Martes</th>
-                  <th className='border-2 border-teal-500 p-3 '>Miércoles</th>
-                  <th className='border-2 border-teal-500 p-3 '>Jueves</th>
-                  <th className='border-2 border-teal-500 p-3 rounded-tr-xl'>Viernes</th>
+                  <th className='border-2 w-10 border-teal-500 p-3 rounded-tl-xl'>Tiempo</th>
+                  <th className='border-2 w-40 border-teal-500 p-3 '>Lunes</th>
+                  <th className='border-2 w-40 border-teal-500 p-3 '>Martes</th>
+                  <th className='border-2 w-36 border-teal-500 p-3 '>Miércoles</th>
+                  <th className='border-2 w-40 border-teal-500 p-3 '>Jueves</th>
+                  <th className='border-2 w-40 border-teal-500 p-3 rounded-tr-xl'>Viernes</th>
                 </tr>
               </thead>
               <tbody className=''>
-                {['7:00', '8:00', '9:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00'].map((time) => (
+                {['7:00', '8:00', '9:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00'].map((time) => (
                   <tr key={time}>
                     <td className='cursor-default	 bg-gray-500 border-2 border-teal-500 text-center'>{time}</td>
                     {['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes'].map((day) => (
                       <td
                         key={day}
-                        className='border-2 border-teal-500 text-center'
+                        className='border-2 p-3 border-teal-500 hover:bg-teal-600 text-center cursor-pointer items-center transition duration-200'
                       >
-                        <div onClick={() => setShowForm(true)} className={`cursor-pointer w-20 h-15 p-3 ${showForm ? 'hover:bg-teal-500' : ''}`}>
-                          <p>Hola</p>
-                        </div>
+                        Hola
                       </td>
                     ))}
                   </tr>
-
                 ))}
-
               </tbody>
             </table>
           </div>
-
         </div>
         <div className="flex w-4/12 ml-16 h-screen">
           {showForm ? (
@@ -266,7 +265,7 @@ export default function Page() {
                     <div className=' ml-1 mr-1 border-2 border-teal-300 rounded-lg bg-gray-500 w-full p-1 hover:bg-teal-900 flex justify-center cursor-pointer'>
                       <p className='text-sm text-teal-300 text-center'>Día seleccionado: </p>
                       <p className='ml-1 text-sm text-teal-300 text-center font-bold'>
-                        {date.getDate()} del {date.getMonth()} de {date.getFullYear()}
+                        {date.getDate()} de {namesMonths[date.getMonth()]} de {date.getFullYear()}
                       </p>
                     </div>) : (
                     <Calendar onChange={setDate}
@@ -280,13 +279,12 @@ export default function Page() {
                   )}
                 </div>
               </div>
-
               <div className='flex-1' >
                 <div className='ml-2 mt-2 mr-2 mb-2 flex items-center justify-center bg-teal-500 rounded-full h-10 cursor-default shadow-lg'>
                   <h1 className='font-black	text-4xl text-teal-800 mr-4'>3</h1>
                   <h1 className=' text-xl font-bold text-teal-800 text-center cursor-default'>Confirmar Turno</h1>
                 </div>
-                {patient ? (
+                {patient && date ? (
                   <div className='mt-4 ml-2 mr-2 mb-2 flex'>
                     <div className='ml-1 mr-1 border-2 border-teal-300 rounded-lg bg-gray-500 w-full p-1'>
                       <h1 className='text-xl font-bold text-teal-100 text-center underline'>Resumen del turno: </h1>
@@ -302,7 +300,7 @@ export default function Page() {
                         <p className='ml-1 text-sm text-teal-100 text-left font-bold'>Año: {date.getFullYear()}</p>
                         <p className='ml-1 text-sm text-teal-100 text-left font-bold'>Para: el Martes {date.getDate()} de {namesMonths[date.getMonth()]}</p>
                       </div>
-                      <div className='flex justify-center items-center text-xl font-semibold transition duration-100 text-teal-800 border-2 rounded-lg ml-2 mr-2 mb-1 p-1 cursor-pointer hover:bg-teal-300 hover:text-teal-900 border-teal-900 bg-teal-500'>
+                      <div onClick={handleSetAppoint} className='flex justify-center items-center text-xl font-semibold transition duration-100 text-teal-800 border-2 rounded-lg ml-2 mr-2 mb-1 p-1 cursor-pointer hover:bg-teal-300 hover:text-teal-900 border-teal-900 bg-teal-500'>
                         Confirmar
                       </div>
                     </div>
@@ -310,7 +308,7 @@ export default function Page() {
                 ) : (
                   <div className='mt-4 ml-2 mr-2 mb-2 flex'>
                     <div className='ml-1 mr-1 border-2 border-teal-300 rounded-lg bg-gray-500 w-full p-1'>
-                      <p className='text-lg font-bold text-teal-300 text-center'>Completa los Items anteriores antes de confirmar el turno</p>
+                      <p className='text-lg font-semibold text-teal-300 text-center'>Completa los Items anteriores antes de confirmar el turno</p>
                     </div>
                   </div>
                 )}
