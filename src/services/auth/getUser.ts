29e2c1f0178ffa6cf03ee6bfd8b92f5d210504c3
ezesanchezb <ezequiel.badenas@uap.edu.ar>
2026-05-06 20:43:01 -0300

    fix(#53): show initials avatar in config and fix photoURL in getUser

--- src/services/auth/getUser.ts
+++ src/services/auth/getUser.ts
@@ -3,24 +3,25 @@ import { get, ref } from "firebase/database";
 import { db, auth } from "@/lib/firebase";
 
 export async function getUser(getOnlyClinicId: boolean) {
-    return new Promise((resolve, reject) => {
-        const unsubscribe = onAuthStateChanged(auth, async (user) => {
-            unsubscribe();
-            if (user) {
-                let dbRef = ref(db, 'admins/' + user.uid);
-                let snapshot = await get(dbRef);
-                if (snapshot.exists()) {
-                    let data = snapshot.val();
-                    data.userUid = user.uid;
-                    if (getOnlyClinicId) {
-                        resolve(data.clinicId);
-                    } else {
-                        resolve(data);
-                    }
-                }
-            } else {
-                reject("User not logged in");
-            }
-        });
+  return new Promise((resolve, reject) => {
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      unsubscribe();
+      if (user) {
+        let dbRef = ref(db, "admins/" + user.uid);
+        let snapshot = await get(dbRef);
+        if (snapshot.exists()) {
+          let data = snapshot.val();
+          data.userUid = user.uid;
+          data.photoURL = `https://firebasestorage.googleapis.com/v0/b/${process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET}/o/userImages%2F${user.uid}.jpg?alt=media`;
+          if (getOnlyClinicId) {
+            resolve(data.clinicId);
+          } else {
+            resolve(data);
+          }
+        }
+      } else {
+        reject("User not logged in");
+      }
     });
+  });
 }
